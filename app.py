eb3d8aa6cc3d42e712602717cd9c810843379ce9
kutuzov_em <kutuzov-1313@yandex.ru>
2021-04-13 18:40:27 +0300

    add SQLALCHEMY_TRACK_MODIFICATIONS = False

--- app.py
+++ app.py
@@ -5,35 +5,10 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///main.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 api = Api(app)
-
-# resources = [
-#     {
-#         "resources": [
-#             {
-#                 "title": "res_1",
-#                 "id": 0,
-#                 "amount": 100,
-#                 "unit": "kg",
-#                 "price": 15,
-#                 "cost": 1500,
-#                 "date": "2022-02-12"
-#             },
-#             {
-#                 "title": "res_2",
-#                 "id": 1,
-#                 "amount": 32,
-#                 "unit": "liter",
-#                 "price": 10,
-#                 "cost": 320,
-#                 "date": "2022-02-12"
-#             }
-#         ],
-#         "total_count": 2
-#     }
-# ]
 
 
 class Warehouse(db.Model):
@@ -77,8 +52,8 @@ class WarehouseResource(Resource):
         resources = Warehouse.query.all()
         return len(posts_schema.dump(resources))
 
-    def update(self, post_id):
-        resource = Warehouse.query.get_or_404(post_id)
+    def patch(self, resource_id):
+        resource = Warehouse.query.get_or_404(resource_id)
 
         if 'title' in request.json:
             resource.title = request.json['title']
@@ -94,14 +69,14 @@ class WarehouseResource(Resource):
         db.session.commit()
         return post_schema.dump(resource)
 
-    def delete(self, post_id):
-        post = Warehouse.query.get_or_404(post_id)
+    def delete(self, resource_id):
+        post = Warehouse.query.get_or_404(resource_id)
         db.session.delete(post)
         db.session.commit()
         return '', 204
 
 
-api.add_resource(WarehouseResource, '/total_cost')
+api.add_resource(WarehouseResource, '/total_cost/<int:post_id>')
 api.add_resource(WarehouseListResource, '/resources')
 post_schema = WarehouseSchema()
 posts_schema = WarehouseSchema(many=True)
